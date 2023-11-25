79f797e715e39be6f0a67ff2bdf6e9e387a9ce70
Daniel20030709 <152022039+Daniel20030709@users.noreply.github.com>
2023-11-25 12:04:14 -0500

    Add files via upload

---
+++ LIBRERIASANTIAGO.py
@@ -0,0 +1,13 @@
+from Atenuacion import AttenuationCalculator
+from Factor_de_potencia import FactorPotencia
+from frecuencia import RCFilterfrecuencia
+from gananciaRC import RCFilterganancia
+from impedancia import RCFilterimpedancia
+from PotenciaActiva import PotenciaActiva
+from PotenciaAparente import PotenciaAparente
+from PotenciaElectrica import PotenciaElectrica
+from PotenciaReactiva import PotenciaReactiva
+from TensionElectrica import TensionElectrica
+
+
+
